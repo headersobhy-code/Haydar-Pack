73a150bced512545f7ec3a7911db63417de2af45
header911 <131144337+header911@users.noreply.github.com>
2026-07-02 04:09:23 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,3 +1,2 @@
 // رابط Google Apps Script /exec الخاص بالمزامنة
-window.HP_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbw0RxMaw2gNicQjSD5T3LHhd-6d2DnABYKGNNMDD1NN3b09wJL3OatLviAn7xqDu2Zq6w/exec';
-window.HP_GITHUB_PAGES_URL = 'https://headersobhy-code.github.io/Haydar-Pack/';
+window.HP_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbztGlxieynseTe3h84ysPesgpuGBYDXmGbtuxb9mjrNyG2W4UVrHs6FsHj57QG7uTTVyg/exec';
