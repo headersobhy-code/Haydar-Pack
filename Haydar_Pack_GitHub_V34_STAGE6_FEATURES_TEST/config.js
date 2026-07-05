0163ca10849f584887dc25aec1de2f37a4afa984
header911 <131144337+header911@users.noreply.github.com>
2026-07-05 04:58:02 +0300

    Delete Haydar_Pack_GitHub_V34_STAGE6_FEATURES_TEST directory

--- Haydar_Pack_GitHub_V34_STAGE6_FEATURES_TEST/config.js
+++
@@ -1,2 +0,0 @@
-// رابط Google Apps Script /exec الخاص بالمزامنة
-window.HP_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbw0RxMaw2gNicQjSD5T3LHhd-6d2DnABYKGNNMDD1NN3b09wJL3OatLviAn7xqDu2Zq6w/exec';
